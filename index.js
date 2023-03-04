0ff2c3a5dfbf7633331fb502517161555c4a4462
sidsingh00 <sidharthsingh2114@gmail.com>
2023-03-04 22:05:52 +0530

    my all topic of portfolio i will upload and will update my system as well

---
+++ index.js
@@ -0,0 +1,10 @@
+//  function demo(){
+//     console.log("open");
+    
+//  }
+//     var link =document.getElementsByClassName(linkdi)
+//         link.click();
+//         link.open('get',"https://www.linkedin.com/in/sidharth-singh-43756a223/",true);
+   
+document.getElementsByClassName(cppc).style.opacity="0.1";
+
